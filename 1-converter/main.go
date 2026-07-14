123bdd939a55fcc1d31a8abc4b02ff5415c67acd
Alexey Suponin <suponin.a@selectel.ru>
2026-07-14 18:31:52 +0700

    first homework: done

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+const usd_eur float64 = 0.88
+const usd_rub float64 = 76.94
+
 func main() {
-	fmt.Println("Hello world")
+	var eur_rub float64 = usd_rub / usd_eur
+	fmt.Println("EUR to RUB =", eur_rub)
 }
