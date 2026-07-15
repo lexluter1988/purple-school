7001abac61ae9f6beea154b1db54539e1de342fa
Alexey Suponin <suponin.a@selectel.ru>
2026-07-15 16:37:14 +0700

    second homework: done

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const usd_eur float64 = 0.88
 const usd_rub float64 = 76.94
 
 func main() {
-	var eur_rub float64 = usd_rub / usd_eur
-	fmt.Println("EUR to RUB =", eur_rub)
+	amount, currency_from, currency_to := getInput()
+	calcExchange(amount, currency_from, currency_to)
+
+}
+
+func getInput() (amount float64, currency_from string, currency_to string) {
+	fmt.Print("Введите количество валюты: ")
+	fmt.Scan(&amount)
+	fmt.Print("Введите название ваше валюты: ")
+	fmt.Scan(&currency_from)
+	fmt.Print("Введите название желаемой валюты: ")
+	fmt.Scan(&currency_to)
+	return
+}
+
+func calcExchange(amount float64, currency_from string, currency_to string) float64 {
+	return 0.0
 }
