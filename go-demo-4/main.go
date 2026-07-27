a9d046f932440392a76bec322e7c7fdeaf90b7a2
Alexey Suponin <suponin.a@selectel.ru>
2026-07-27 23:42:58 +0700

    struct begining

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
+func main() {
+	login := promptData("Введите логин: ")
+	password := promptData("Введите пароль: ")
+	url := promptData("Введите URL: ")
+
+	account := account{
+		password: password,
+		login:    login,
+		url:      url,
+	}
+
+	outputPassword(&account)
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt)
+	var res string
+	fmt.Scan(&res)
+	return res
+}
+
+func outputPassword(acc *account) {
+	fmt.Printf("Ваш логин: %s, ваш пароль: %s, ваш URL: %s", (*acc).login, acc.password, acc.url)
 }
